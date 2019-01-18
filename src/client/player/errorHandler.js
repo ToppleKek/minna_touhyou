a5358569bee5460906e1d3ecc6bbd3df8f967d43
braebear85 <braebear85@gmail.com>
2019-01-18 16:42:50 -0500

    small

--- src/client/player/errorHandler.js
+++ src/client/player/errorHandler.js
@@ -1,3 +1,19 @@
 function handleReconnectError(error) {
-	
+    toastr.error(`WebSocket received error while trying to reconnect: ${error.message}`, 'WebSocket Error Handler');
+}
+
+function handleReconnecting(attempt) {
+
+}
+
+function handleConnectionError(error) {
+
+}
+
+function handleGenericSocketError(error) {
+
+}
+
+function handleReconnectAttempt(attempt) {
+
 }
